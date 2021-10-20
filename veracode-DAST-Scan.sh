cb656bd4ea4fae44a43ff9bb3ab6c6448a2919f7
orodriguez <orodriguez@veracode.com>
2021-10-20 15:43:19 -0500

    Separando el HMAC signing

--- veracode-DAST-Scan.sh
+++ veracode-DAST-Scan.sh
@@ -15,23 +15,23 @@
         echo ''
 
         #Create Credentials File
-        echo '[INFO] ------------------------------------------------------------------------'
-        echo '[INFO] --- CONFIGURING API CREDENTIALS FILE...'
-        echo '[INFO] ------------------------------------------------------------------------'
-        echo '[default]' >> credentials.txt
-        echo 'veracode_api_key_id='$1 >> credentials.txt
-        echo 'veracode_api_key_secret='$2 >> credentials.txt
-        mkdir /home/vsts/.veracode/
-        cp -f credentials.txt /home/vsts/.veracode/credentials
-        chmod 755 /home/vsts/.veracode/credentials
-        echo ''
+#        echo '[INFO] ------------------------------------------------------------------------'
+#        echo '[INFO] --- CONFIGURING API CREDENTIALS FILE...'
+#        echo '[INFO] ------------------------------------------------------------------------'
+#        echo '[default]' >> credentials.txt
+#        echo 'veracode_api_key_id='$1 >> credentials.txt
+#        echo 'veracode_api_key_secret='$2 >> credentials.txt
+#        mkdir /home/vsts/.veracode/
+#        cp -f credentials.txt /home/vsts/.veracode/credentials
+#        chmod 755 /home/vsts/.veracode/credentials
+#        echo ''
 
         #Install Veracode Authentication Library
-        echo '[INFO] ------------------------------------------------------------------------'
-        echo '[INFO ] --- INSTALLING VERACODE AUTHENTICATION LIBRARY...'
-        echo '[INFO] ------------------------------------------------------------------------'
-        pip install veracode-api-signing || echo "[ERROR] There was a problem Installing Veracode Authentication Library..." | exit 1
-        echo ''
+#        echo '[INFO] ------------------------------------------------------------------------'
+#        echo '[INFO ] --- INSTALLING VERACODE AUTHENTICATION LIBRARY...'
+#        echo '[INFO] ------------------------------------------------------------------------'
+#        pip install veracode-api-signing || echo "[ERROR] There was a problem Installing Veracode Authentication Library..." | exit 1
+#        echo ''
 
         echo '[INFO] ------------------------------------------------------------------------'
         echo '[INFO] --- CREATING JSON FILE...'
