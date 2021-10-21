79c27e0d3a29bd8e518c9ae775419c621db1cdd2
orodriguez <orodriguez@veracode.com>
2021-10-21 08:26:01 -0500

    Se elimina autenticacion HMAC del script de lanzamiento de DAST

--- veracode-DAST-Scan.sh
+++ veracode-DAST-Scan.sh
@@ -1,9 +1,7 @@
 #/bin/bash
 
-        #$1 VID
-        #$2 VKEY
-        #$3 DAST_SCAN_NAME
-        #$4 APP_URL
+        #$1 DAST_SCAN_NAME
+        #$2 APP_URL
 
         echo ''
         echo '====== DEBUG START ======'
@@ -14,35 +12,16 @@
         echo '====== DEBUG END ======'
         echo ''
 
-        #Create Credentials File
-#        echo '[INFO] ------------------------------------------------------------------------'
-#        echo '[INFO] --- CONFIGURING API CREDENTIALS FILE...'
-#        echo '[INFO] ------------------------------------------------------------------------'
-#        echo '[default]' >> credentials.txt
-#        echo 'veracode_api_key_id='$1 >> credentials.txt
-#        echo 'veracode_api_key_secret='$2 >> credentials.txt
-#        mkdir /home/vsts/.veracode/
-#        cp -f credentials.txt /home/vsts/.veracode/credentials
-#        chmod 755 /home/vsts/.veracode/credentials
-#        echo ''
-
-        #Install Veracode Authentication Library
-#        echo '[INFO] ------------------------------------------------------------------------'
-#        echo '[INFO ] --- INSTALLING VERACODE AUTHENTICATION LIBRARY...'
-#        echo '[INFO] ------------------------------------------------------------------------'
-#        pip install veracode-api-signing || echo "[ERROR] There was a problem Installing Veracode Authentication Library..." | exit 1
-#        echo ''
-
         echo '[INFO] ------------------------------------------------------------------------'
         echo '[INFO] --- CREATING JSON FILE...'
         echo '[INFO] ------------------------------------------------------------------------'
         echo  '{' >> da_scan.json
-        echo  '  "name": "'$3'",' >> da_scan.json
+        echo  '  "name": "'$1'",' >> da_scan.json
         echo  '  "scans": [' >> da_scan.json
         echo  '    {' >> da_scan.json
         echo  '      "scan_config_request": {' >> da_scan.json
         echo  '        "target_url": {' >> da_scan.json
-        echo  '          "url": "'$4'"' >> da_scan.json
+        echo  '          "url": "'$2'"' >> da_scan.json
         echo  '        }' >> da_scan.json
         echo  '      }' >> da_scan.json
         echo  '    }' >> da_scan.json
@@ -60,6 +39,9 @@
 
         #Launch DAST Scan by Using APIs
         echo '[INFO] ------------------------------------------------------------------------'
-        echo '[INFO] LAUNCHING DAST SCAN '$3
+        echo '[INFO] LAUNCHING DAST SCAN '$1
+        echo '[INFO] ------------------------------------------------------------------------'
         http --auth-type=veracode_hmac "https://api.veracode.com/was/configservice/v1/analyses" < da_scan.json || echo "[ERROR] There was a problem launching the DAST Scan..." | exit 1
+        echo '[INFO Scan launched successfully!]'
+        echo '[INFO] ------------------------------------------------------------------------'
         
