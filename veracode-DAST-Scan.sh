eba653c08da7aeffb143d9f92edc044e74570502
orodriguez <orodriguez@veracode.com>
2022-11-21 15:12:38 -0500

    sdfgsdfgs

--- veracode-DAST-Scan.sh
+++ veracode-DAST-Scan.sh
@@ -40,7 +40,7 @@
         echo '[INFO] LAUNCHING DAST SCAN '$1
         echo '[INFO] ------------------------------------------------------------------------'
         #http --auth-type=veracode_hmac "https://api.veracode.com/was/configservice/v1/analyses" < da_scan.json || echo "[ERROR] There was a problem launching the DAST Scan..." | exit 1
-        RESPONSE='http --auth-type=veracode_hmac "https://api.veracode.com/was/configservice/v1/analyses" < da_scan.json'
+        RESPONSE=`http --auth-type=veracode_hmac "https://api.veracode.com/was/configservice/v1/analyses" < da_scan.json`
         echo $RESPONSE
         #HTTP_CODE_RESPONSE=`echo $RESPONSE | grep -Po '"httpcode":(\W+)?"\K[a-zA-Z0-9._]+(?=")'`
         #HTTP_CODE_RESPONSE='echo $RESPONSE | grep -Po '"httpcode":(\W+)?"\K[a-zA-Z0-9._]+(?=")'
